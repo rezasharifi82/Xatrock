ce8fa4dfb52b0222c0761b5b09bdf55cca6992f9
salehmhosseini <97988967+salehmhosseini@users.noreply.github.com>
2022-06-25 13:51:08 +0430

    Graphic folder

---
+++ Graphic/Xatrock/DataBase/Activity_Source.java
@@ -0,0 +1,46 @@
+package com.DataBase;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Date;
+import java.util.List;
+
+public class Activity_Source {
+    public static Activity me;
+
+    public static void save_activity(int mid, String name, Date deadline , int state) {
+        me=new Activity();
+        me.setActname(name);
+        me.setMemberid(mid);
+        me.setState(state);
+        me.setDeadline(deadline);
+        Configuration con=new Configuration().configure().addAnnotatedClass(Activity.class);
+        SessionFactory sf= con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx=session.beginTransaction();
+        session.save(me);
+        tx.commit();
+    }
+
+    public static void save_activity(List<Integer> ibm,String name,Date dead,int state ) {
+        me=new Activity();
+
+        Configuration con=new Configuration().configure().addAnnotatedClass(Activity.class);
+        SessionFactory sf= con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx=session.beginTransaction();
+        for(Integer i:  ibm){
+            me.setMemberid(i);
+            me.setActname(name);
+            me.setDeadline(dead);
+            me.setState(state);
+            session.save(me);
+            me=new Activity();
+        }
+
+        tx.commit();
+    }
+}
