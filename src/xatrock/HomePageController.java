eb259ff60c4f4267264dd3b530c99ad2c3178b10
Saleh <salehmhosseini2002@gmail.com>
2022-06-26 14:25:08 +0430

    new change

---
+++ src/xatrock/HomePageController.java
@@ -0,0 +1,40 @@
+
+package xatrock;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.ImageView;
+
+
+public class HomePageController implements Initializable {
+
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private Button messengertButton;
+    @FXML
+    private ImageView logoutLabel;
+    @FXML
+    private Button mathematicButton;
+    @FXML
+    private Button physicButton;
+    @FXML
+    private Button logicalCircuitButton;
+    @FXML
+    private Button programmingButton;
+    @FXML
+    private ImageView clock;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
